486d4c70877b990ca0ad4bf1d9269b3b1d2fd20b
Mahesh-1304 <ubarhandemahesh2004@gmail.com>
2025-06-10 21:55:13 +0530

    Fix merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // or whatever you want — just remove the <<<<<<< stuff
   plugins: [react()],
-<<<<<<< HEAD
-  base:"/",
-=======
-  base:"/My-Portfolio/",
->>>>>>> fbb2d2ecc26ac19537412aa150f320fc48621f18
-})
+});
